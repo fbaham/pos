f68135df1ae664a93dc22dd68ed4256511db48f0
strengthtoendure <francisco.bahamondes90@gmail.com>
2018-04-07 15:15:57 -0300

    pagina login

--- views/modules/login.php
+++ views/modules/login.php
@@ -1,3 +1,4 @@
+<div id="back"></div>
 <div class="login-box">
   <div class="login-logo">
     <img src="views/img/template/logo-blanco-bloque.png" class="img-responsive" style="padding:30px 100px 0px 100px">
@@ -8,42 +9,18 @@
 
     <form method="post">
       <div class="form-group has-feedback">
-        <input type="email" class="form-control" placeholder="Email">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <input type="text" class="form-control" placeholder="Usuario" name="user" requided>
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" class="form-control" placeholder="Password">
+        <input type="password" class="form-control" placeholder="Contraseña" name="pass" required>
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox"> Remember Me
-            </label>
-          </div>
-        </div>
-        <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          <button type="submit" class="btn btn-primary btn-flat">Iniciar sesión</button>
         </div>
-        <!-- /.col -->
       </div>
     </form>
-
-    <div class="social-auth-links text-center">
-      <p>- OR -</p>
-      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-        Facebook</a>
-      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-        Google+</a>
-    </div>
-    <!-- /.social-auth-links -->
-
-    <a href="#">I forgot my password</a><br>
-    <a href="register.html" class="text-center">Register a new membership</a>
-
   </div>
-  <!-- /.login-box-body -->
 </div>
-<!-- /.login-box -->
--- views/template.php
+++ views/template.php
@@ -22,6 +22,8 @@
   <link rel="stylesheet" href="views/dist/css/skins/_all-skins.min.css">
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <!-- Estilos propios -->
+  <link rel="stylesheet" href="views\css\styles.css">
   <!--==================
   PLUGINS DE JAVASCRIPT
   ===================-->
